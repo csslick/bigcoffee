a6b17f56be7e4b907f32b4788c59f8978cc81ff9
csslick <digitone@naver.com>
2021-01-28 15:24:59 +0900

    메인메뉴 열기 기능 추가

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/* main.js */
+$(function(){
+  // 메인메뉴 열기
+  $('.btn_toggle').on('click', function(){
+    $('#gnb').show();
+  })
+
+  // 메인메뉴 닫기
+  $('.btn_close').on('click', function(){
+    $('#gnb').hide();
+  });
+
+});
