c216528d946c94a2b269ceefbf711fbf1d7a800b
csslick <digitone@naver.com>
2021-01-28 16:29:05 +0900

    메인 비주얼 슬라이더 전환 효과

--- js/main.js
+++ js/main.js
@@ -10,4 +10,16 @@ $(function(){
     $('#gnb').hide();
   });
 
+  /*** 이미지 슬라이더: fade 효과(전환)로 할것임 ****/
+  let num = 1;
+  let slider = setInterval(function(){
+    if(num > 2) { num = 0; }
+    // 전체 이미지를 fade out
+    $('.slider > img').removeClass('show');
+    // 다음 이미지 표시
+    $('.slider > img').eq(num).addClass('show');
+    console.log(num)
+    num++;
+  }, 3000);
+
 });
