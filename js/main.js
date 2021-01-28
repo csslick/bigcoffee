b720fd1593a54d32d901f3152a796861dfe1cd56
csslick <digitone@naver.com>
2021-01-28 17:50:56 +0900

    오시는 길 서브페이지 추가

--- js/main.js
+++ js/main.js
@@ -11,15 +11,16 @@ $(function(){
   });
 
   /*** 이미지 슬라이더: fade 효과(전환)로 할것임 ****/
-  let num = 1;
+  let num = 1; // 이미지 번호
   let slider = setInterval(function(){
+    // 마지막 이미지이면 처음 이미지로
     if(num > 2) { num = 0; }
     // 전체 이미지를 fade out
     $('.slider > img').removeClass('show');
     // 다음 이미지 표시
     $('.slider > img').eq(num).addClass('show');
     console.log(num)
-    num++;
-  }, 3000);
+    num++;  // 다음 이미지 번호
+  }, 5000);
 
 });
